58002d4ec3dd509713b6946829b4e70e055a6e94
walkvoid <2976147177@qq.com>
2023-09-12 17:54:25 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# c-learn
-learn c language
+# c-learn: learn c language; 学习C语言
+
+### 项目结构
+```
+|
+|--.out      #compile file category
+|--.vscode   #vscode config
+|--doc       # document
+|--src       # resource code category
+|--README.md
+```
+
+### 文档导航
+#### [👉👉👉环境配置](doc/环境配置.md)
---
+++ src/def/primitiveTypeDef.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    short short1 = 10;
+    printf(":::::::short:%hd\n",short1);
+    // char char1 = 46; ${fileBasenameNoExtension}
+    // int int1 = 10;
+    // long long1 = 443L;  
+    // float float1 = 1.2f;
+    // double double1 = 1.8f;
+    //size_t st1 = 10L;
+    // long int li1 = 1;
+    printf("hellow walkvoid...\n");
+    printf("hellow walkvoid...");
+    /* code */
+    return 0;
+}
